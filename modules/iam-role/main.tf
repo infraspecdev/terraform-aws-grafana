75b1b4526536fe5074a9704dd33b9b5d05fa7f72
saumitra-dev <saumitra@infraspec.dev>
2024-08-19 18:03:51 +0530

    fix(modules/iam-role): missing json encoding step for assume role policy

--- modules/iam-role/main.tf
+++ modules/iam-role/main.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "this" {
   name               = var.name
   description        = var.description
-  assume_role_policy = var.assume_role_policy
+  assume_role_policy = jsonencode(var.assume_role_policy)
 
   tags = var.tags
 }
