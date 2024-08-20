d16835e0d6d62c28e216b55fdee09d9ad61e1d1c
saumitra-dev <saumitra@infraspec.dev>
2024-08-20 11:29:45 +0530

    feat(modules): route-53-record sub-module
    Co-authored-by: rohit@infraspec.dev <rohit@infraspec.dev>

---
+++ modules/route-53-record/variables.tf
@@ -0,0 +1,14 @@
+variable "domain" {
+  description = "Domain name"
+  type        = string
+}
+
+variable "alb_dns_name" {
+  description = "ALB DNS name"
+  type        = string
+}
+
+variable "alb_zone_id" {
+  description = "ALB zone ID"
+  type        = string
+}
